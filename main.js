8086e368ce344e0b1355b9d3d82746519cea6005
molarmanful <molarmanful@users.noreply.github.com>
2014-06-01 16:10:25 -0700

    Update main.js

--- main.js
+++ main.js
@@ -44,9 +44,9 @@ $(document).ready(function(){
 			cubelet.addressY * distance,
 			cubelet.addressZ * distance
 		)
-		if( cubelet.type === 'core'   ) $(cubelet).remove()
-		if( cubelet.type === 'center' ) $(cubelet).remove()
 		var delay
+		if( cubelet.type === 'core'   ) delay = 0
+		if( cubelet.type === 'center' ) delay = 0
 		if( cubelet.type === 'edge'   ) delay = (  800 ).random( 1000 )
 		if( cubelet.type === 'corner' ) delay = ( 1100 ).random( 1500 )
 		new TWEEN.Tween( cubelet.position )
@@ -66,86 +66,9 @@ $(document).ready(function(){
 			.start( cube.time )
 
 		cubelet.isTweening = true
+		if( cubelet.type === 'core'   ) $(cubelet).remove()
+		if( cubelet.type === 'center' ) $(cubelet).remove()
 	})
-	var 
-	W = ERNO.WHITE,
-	O = ERNO.ORANGE,
-	B = ERNO.BLUE,
-	R = ERNO.RED,
-	G = ERNO.GREEN,
-	Y = ERNO.YELLOW
-
-	;[
-
-		//  Front slice
-
-		[ W, R,  ,  , B,   ],    [ W, R,  ,  ,  ,   ],    [ W, R, G,  ,  ,   ],//   0,  1,  2
-		[ W,  ,  ,  , B,   ],    [ W,  ,  ,  ,  ,   ],    [ W,  , G,  ,  ,   ],//   3,  4,  5
-		[ W,  ,  , O, B,   ],    [ W,  ,  , O,  ,   ],    [ W,  , G, O,  ,   ],//   6,  7,  8
-
-
-		//  Standing slice
-
-		[  , R,  ,  , B,   ],    [  , R,  ,  ,  ,   ],    [  , R, G,  ,  ,   ],//   9, 10, 11
-		[  ,  ,  ,  , B,   ],    [  ,  ,  ,  ,  ,   ],    [  ,  , G,  ,  ,   ],//  12, XX, 14
-		[  ,  ,  , O, B,   ],    [  ,  ,  , O,  ,   ],    [  ,  , G, O,  ,   ],//  15, 16, 17
-
-
-		//  Back slice
-
-		[  , R,  ,  , B, Y ],    [  , R,  ,  ,  , Y ],    [  , R, G,  ,  , Y ],//  18, 19, 20
-		[  ,  ,  ,  , B, Y ],    [  ,  ,  ,  ,  , Y ],    [  ,  , G,  ,  , Y ],//  21, 22, 23
-		[  ,  ,  , O, B, Y ],    [  ,  ,  , O,  , Y ],    [  ,  , G, O,  , Y ] //  24, 25, 26
-
-	].forEach( function( cubeletColorMap, cubeletId ){
-
-		cubeletColorMap.forEach( function( faceColor, faceIndex ){
-
-			var c = cube.cubelets[ cubeletId ].faces[ faceIndex ].element.querySelector( '.sticker' ).classList
-			
-			while( c.length ) c.remove( c[ 0 ])
-			c.add( 'sticker', faceColor.name )
-			cube.cubelets[ cubeletId ].faces[ faceIndex ].color = faceColor
-		})
-	})
-	cube.object3D.children.forEach( function( child ){
-
-		if( child instanceof THREE.CSS3DObject && child.element.classList.contains( 'faceLabel' )){
-
-			child.position.multiplyScalar( 0.8 )
-		}
-	})
-	delete cube.showFaceLabels
-	delete cube.hideFaceLabels
-	ERNO.Cubelet.prototype.setOpacity = function( opacityTarget, onComplete ){
-
-		if( this.opacityTween ) this.opacityTween.stop()
-		if( opacityTarget === undefined ) opacityTarget = 1
-		if( opacityTarget !== this.opacity ){
-
-			var 
-			that = this,
-			tweenDuration = ( opacityTarget - this.opacity ).absolute().scale( 0, 1, 0, this.cube.opacityTweenDuration )
-
-			this.opacityTween = new TWEEN.Tween({ opacity: this.opacity })
-				.to({
-
-					opacity: opacityTarget
-				
-				}, tweenDuration )
-				.easing( TWEEN.Easing.Quadratic.InOut )
-				.onUpdate( function(){
-
-					that.css3DObject.element.style.opacity = this.opacity
-					that.opacity = this.opacity
-				})
-				.onComplete( function(){
-
-					if( onComplete instanceof Function ) onComplete()
-				})
-				.start( cube.time )
-		}
-	}
   var container = $( '#containerforcube' );
   container.append( cube.domElement );
   $('button').click(function(){
