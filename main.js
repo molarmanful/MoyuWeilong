bcf0d6289c416ae09bac025f28ab05998102dbc8
molarmanful <molarmanful@users.noreply.github.com>
2014-06-01 14:40:53 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   window.cube = new ERNO.Cube();
   var container = $( '#containerforcube' );
   container.append( cube.domElement );
+  $('button').click(function(){
+    $('#directions').slideToggle();
+  });
 });
