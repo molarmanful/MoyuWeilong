2e653923c4f8b7763d7229ad8dcf8b0f9421f5d1
molarmanful <molarmanful@users.noreply.github.com>
2014-06-01 15:08:48 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   window.cube = new ERNO.Cube();
   var container = $( '#containerforcube' );
   container.append( cube.domElement );
+  if(cubelet.type = 'center'){
+    cubelet.remove();
+  }
   $('button').click(function(){
     $('#directions').slideToggle();
   });
