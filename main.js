e45625d5079ebfc8a24a4d0d5cc125b6a82fd8b9
molarmanful <molarmanful@users.noreply.github.com>
2014-08-20 16:41:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
 	window.cube = new ERNO.Cube();
 	cube.position.y = 0;
+	var white = false;
   var container = $( '#containerforcube' );
   container.append( cube.domElement );
   $('#togdir').click(function(){
-    $('#directions').slideToggle();
+  	$('#directions').slideToggle();
+  });
+  $('changebgcolor').click(function(){
+  	if(white = true){
+  		$('.cube .face').css('background-color', 'black');
+  		var white = false;
+  	} else {
+  		$('.cube .face').css('background-color', 'white');
+  		var white = true;
+  	}
   });
 });
