61330d6c8d3c10cc0e3d4fdaffed1d2dff95d454
molarmanful <molarmanful@users.noreply.github.com>
2014-08-21 11:09:27 -0700

    Update main.js

--- main.js
+++ main.js
@@ -23,9 +23,12 @@ $(document).ready(function(){
   });
   $('#demo').click(function(){
   	cube.twist('mmeess');
+  	$('#demotext').text('I am the Moyu Weilong.');
   	cube.twist('mmeess');
+  	$('#demotext').text('I am smooth, fast, and $13.');
   	cube.twist('meME');
+  	$('#demotext').text('I am Chinese.');
   	cube.twist('MEme');
-  	
+  	$('#demotext').text('');
   });
 });
