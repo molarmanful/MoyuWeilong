3f8d13bd434db5489f32b5d625828b5bc85e351e
molarmanful <molarmanful@users.noreply.github.com>
2014-11-02 11:05:03 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var idleInterval = setInterval(timerIncrement, 10000);
 $(document).ready(function(){
+  scramblers['333'].initialize(null, Math);
   if ( self !== top ) {
     $('#dircon').css('display', 'none');
   }
