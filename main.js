5083d83d6fdecc94abeb3fb97d2e6575871b307b
molarmanful <molarmanful@users.noreply.github.com>
2014-11-02 20:27:15 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 var idleInterval = setInterval(timerIncrement, 10000);
 $(document).ready(function(){
+  // update 'data-posit' when twisted
+        cube.addEventListener('onTwistComplete', function(e) {
+        	var posit = '';
+        	var colormap = {};
+        	colormap[cube.up.cubelets[4].up.color.initial] = 'U';
+        	colormap[cube.left.cubelets[4].left.color.initial] = 'L';
+        	colormap[cube.front.cubelets[4].front.color.initial] = 'F';
+        	colormap[cube.right.cubelets[4].right.color.initial] = 'R';
+        	colormap[cube.back.cubelets[4].back.color.initial] = 'B';
+        	colormap[cube.down.cubelets[4].down.color.initial] = 'D';
+        	[8, 7, 6, 5, 4, 3, 2, 1, 0].forEach(function(i) {
+        		posit += colormap[cube.up.cubelets[i].up.color.initial];
+        	});
+            	[8, 7, 6, 5, 4, 3, 2, 1, 0].forEach(function(i) {
+                	posit += colormap[cube.right.cubelets[i].right.color.initial];
+        	});
+        	[8, 7, 6, 5, 4, 3, 2, 1, 0].forEach(function(i) {
+        	        posit += colormap[cube.front.cubelets[i].front.color.initial];
+	        });
+                [2, 5, 8, 1, 4, 7, 0, 3, 6].forEach(function(i) {
+        	        posit += colormap[cube.down.cubelets[i].down.color.initial];
+                });
+                [6, 3, 0, 7, 4, 1, 8, 5, 2].forEach(function(i) {
+        	        posit += colormap[cube.left.cubelets[i].left.color.initial];
+        	});
+        	[6, 3, 0, 7, 4, 1, 8, 5, 2].forEach(function(i) {
+        	        posit += colormap[cube.back.cubelets[i].back.color.initial];
+        	});
+        	window.document.body.setAttribute('data-posit', posit);
+        });
   scramblers['333'].initialize(null, Math);
   if ( self !== top ) {
     $('#dircon').css('display', 'none');
@@ -64,6 +94,9 @@ $(document).ready(function(){
 	$(this).css('display', 'none');
 	$('#two').css('display', 'inline-block');
   });
+  $('#solve').click(function(){
+  	dosolve();
+  });
   $('button').click(function(){
   	$(this).blur();
   });
@@ -78,4 +111,22 @@ function timerIncrement() {
     clearInterval(idleInterval);
     cube.autoRotate = false;
   });
+}
+function dosolve(){
+	var posit = document.body.getAttribute('data-posit');
+        var solution = scramblers['333'].solution(posit);
+        console.log(solution);
+        var twist = '';
+        solution.trim().split(/\s+/).reverse().forEach(function(e) {
+                if (e.length === 2) {
+                        if (e[1] === '2') {
+                            twist += e[0] + e[0];
+                        } else {
+                            twist += e[0];
+                        }
+                } else {
+                        twist += e[0].toLowerCase();
+                }
+        });
+        cube.twist(solution);
 }
